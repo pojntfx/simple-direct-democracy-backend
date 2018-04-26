9f744f895dcc148d962184cfb565444362c5490b
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-04-26 20:16:54 +0200

    Removed Typescript, added basic create operation

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { SimpleDirectDemocracyBackend } from "./start";
-
-const simpleDirectDemocracyBackend: SimpleDirectDemocracyBackend = new SimpleDirectDemocracyBackend();
-
-simpleDirectDemocracyBackend.start({ port: 3000 });
