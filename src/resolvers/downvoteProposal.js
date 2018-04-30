cad8b9536ad90252021757cd5ba749f3bd53e323
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-04-30 18:14:09 +0200

    Refactored proposal downvote checks to check whether the user has downvoted before

--- src/resolvers/downvoteProposal.js
+++ src/resolvers/downvoteProposal.js
@@ -5,35 +5,41 @@ import { createError } from "apollo-errors";
 
 export const downvoteProposal = async ({ id, author }) => {
   try {
-    return await Proposal.findOne({ author }).then(
-      ({ author }) =>
+    await Proposal.findOne({ _id: id }).then(
+      res =>
+        //   console.log(
+        //     `Requester: ${author}
+        //           Author: ${res.author}
+        //           Author is requester: ${!(res.author === author)}
+        //           Downvoters: ${res.downvoters}
+        //           Is downvoter: ${!res.downvoters.includes(author)}
+        //           Should be updated: ${!(res.author === author) &&
+        //             !res.downvoters.includes(author)}`
+        //   )
+        // );
         // You shouldn't be able to upvote your own proposals
-        author === author
-          ? Promise.reject("You can't vote on your own proposal")
-          : updateAndReturn(id)
+        !(res.author === author) && !res.downvoters.includes(author)
+          ? Proposal.findOneAndUpdate(
+              { _id: id },
+              { $inc: { votes: -1 }, $push: { downvoters: author } },
+              { new: true },
+              (err, proposal) => {
+                err
+                  ? console.log(err)
+                  : pubsub.publish(UPDATED_PROPOSAL_TOPIC, {
+                      updatedProposals: allProposals()
+                    }),
+                  proposal;
+              }
+            )
+          : Promise.reject(
+              "You can't vote on your own proposal nor can you vote multiple times!"
+            )
     );
   } catch (error) {
-    const AuthorTriedToVoteOnOwnProposalError = createError(
-      "AuthorTriedToVoteOnOwnProposalError",
-      {
-        message: error
-      }
-    );
-    throw new AuthorTriedToVoteOnOwnProposalError();
+    const IllegalVoteError = createError("IllegalVoteError", {
+      message: error
+    });
+    throw new IllegalVoteError();
   }
 };
-
-const updateAndReturn = ({ id }) =>
-  Proposal.findOneAndUpdate(
-    { _id: id },
-    { $inc: { votes: -1 } },
-    { new: true },
-    (err, proposal) => {
-      err
-        ? console.log(err)
-        : pubsub.publish(UPDATED_PROPOSAL_TOPIC, {
-            updatedProposals: allProposals()
-          }),
-        proposal;
-    }
-  );
